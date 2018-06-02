6fb6504ebb4daa95db4bb71a515e454b93469505
inshiro <peterhalim@live.com>
2018-06-02 01:28:53 -0700

    Fix travis build scripts

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,18 +3,18 @@
 git fetch --unshallow #required for commit count
 
 if [ -z "$TRAVIS_TAG" ]; then
-    ./gradlew clean assembleStandardDebug
+    ./gradlew clean assembleDebug
 
     COMMIT_COUNT=$(git rev-list --count HEAD)
     export ARTIFACT="kitvei-r${COMMIT_COUNT}.apk"
 
-    mv app/build/outputs/apk/standard/debug/app-standard-debug.apk $ARTIFACT
+    mv app/build/outputs/apk/debug/app-debug.apk $ARTIFACT
 else
-    ./gradlew clean assembleStandardRelease
+    ./gradlew clean assembleRelease
 
     TOOLS="$(ls -d ${ANDROID_HOME}/build-tools/* | tail -1)"
     export ARTIFACT="kitvei-${TRAVIS_TAG}.apk"
 
-    ${TOOLS}/zipalign -v -p 4 app/build/outputs/apk/standard/release/app-standard-release-unsigned.apk app-aligned.apk
+    ${TOOLS}/zipalign -v -p 4 app/build/outputs/apk/standard/app-release-unsigned.apk app-aligned.apk
     ${TOOLS}/apksigner sign --ks $STORE_PATH --ks-key-alias $STORE_ALIAS --ks-pass env:STORE_PASS --key-pass env:KEY_PASS --out $ARTIFACT app-aligned.apk
 fi
